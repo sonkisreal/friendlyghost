ea5d5abf954b2d6df94bb2e0c01be03ab2364e8e
sonkisreal <thisplayerverysus1234@gmail.com>
2026-07-05 16:00:24 +0700

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // Đảm bảo dùng plugin đúng phiên bản mới, không dùng loại lỗi cũ nữa
-
-export default defineConfig({
-  base: '/', // Giữ nguyên là '/' vì bạn đang dùng tên miền riêng (Custom Domain)
-  plugins: [react()],
-})
